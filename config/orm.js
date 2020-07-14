85b1e1f443de92d0418199c149a74dc8eeb818d4
John Robling <roblingjohn@gmail.com>
2020-07-13 21:36:39 -0400

    setn functions on orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,32 @@
 var connection = require("../config/connection.js");
 
-selectAll()
-
-insertOne()
-
-updateOne()
+const orm = {
+    selectAll: function(cb) {
+        let queryString = "SELECT * FROM burgers;";
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+          cb(result);
+        });
+},
+    insertOne: function(burger_name, cb){
+        connection.query('INSERT INTO burgers (burger_name) VALUES ?;', [burger_name], function(err, result) {
+            if (err) {
+              throw err;
+            }
+      
+            cb(result);
+          });
+        },
+    updateOne: function(id, cb){
+        connection.query('UPDATE burgers SET devoured = true WHERE id = ?', id, function(err, result) {
+            if (err) {
+                throw err;
+            }
+        cb(result);
+        })
+    }        
+}
 
 module.exports = orm;
